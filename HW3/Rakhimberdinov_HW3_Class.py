5428814e2d1664ed1b38ef8d709d91eca2e0805b
Daniyar-R <100212201+Daniyar-R@users.noreply.github.com>
2022-10-14 01:23:40 +0200

    Update Rakhimberdinov_HW3_Class.py
    refactoring + docstring

--- HW3/Rakhimberdinov_HW3_Class.py
+++ HW3/Rakhimberdinov_HW3_Class.py
@@ -1,33 +1,67 @@
 class CountVectorizer:
+    """
+    A class used to convert a collection of text documents to a matrix of token counts.
+
+    ...
+
+    Attributes
+    ----------
+    lowercase : bool
+        whether to apply lower case to words or not
+        default is True
+    feature_names : set
+        all unique tokens in documents
+
+    Methods
+    -------
+    lower()
+        Return a list of lowered strings
+
+    fit_transform(self, container):
+        Assign feature names
+        Return a matrix of token counts
+
+    get_feature_names(self)
+        Return all unique tokens in documents
+    """
     def __init__(self, lowercase=True):
-        self.lowercase = lowercase
-        self.tokens = []
+        self.lowercase_flag = lowercase
+        self.feature_names = set()
 
     @staticmethod
     def lower(container_to_lower):
+        """
+        :param container_to_lower:
+            default is True
+        :return: a list of lowered strings
+        """
         return [x.lower() for x in container_to_lower]
 
-    def fit_transform(self, contain_param):
-        if self.lowercase:
-            container = self.lower(contain_param)
-        else:
-            container = contain_param
+    def fit_transform(self, container):
+        """
+        :param container:
+        :return: a matrix of token counts
+        """
+        container = self.lower(container) if self.lowercase_flag else container
+        container_split = [document.split() for document in container]
 
-        all_words = [a for i in container for a in i.split()]
+        self.feature_names = set(word for document in container_split for word in document)
         counter_matrix = []
-        for sent in container:
-            counter = dict().fromkeys(all_words, 0)
-            for w in sent.split():
-                counter[w] = counter.get(w) + 1
-            counter_matrix.append([*counter.values()])
 
-        self.tokens = [*dict().fromkeys(all_words, 0).keys()]
+        for document in container_split:
+            counter = dict().fromkeys(self.feature_names, 0)
+            for word in document:
+                counter[word] = counter[word] + 1
+            counter_matrix.append([*counter.values()])
 
         return counter_matrix
 
-    # this method is optional because there is the same attribute
     def get_feature_names(self):
-        return self.tokens
+        """
+        :return: all unique tokens in documents
+        """
+        return self.feature_names
+
 
 if __name__ == '__main__':
     corpus = ['Crock Pot Pasta Never boil pasta again', 'Pasta Pomodoro Fresh ingredients Parmesan to taste']
